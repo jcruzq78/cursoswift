baa8fef400ed6e80af5dbb267dea68bb35e87494
Jorge Manuel Cruz Quetzal <jcruzq78@gmail.com>
2016-05-02 01:25:06 -0500

    Adding some changes to juegoMemoria playground

--- juegoMemoria.playground/Contents.swift
+++ juegoMemoria.playground/Contents.swift
@@ -2,4 +2,21 @@
 
 import UIKit
 
-
+for i in 0...100{
+    
+    if i >= 30 && i <= 40 {
+        print("#\(i) Viva Swift!!!")
+    }
+    
+    if i > 0 {
+        if i > 0 && i % 5 == 0 {
+            print("#\(i) Bingo!!!")
+        }
+        
+        if i % 2 == 0 {
+            print("#\(i) par!!!")
+        } else {
+            print("#\(i) impar!!!")
+        }
+    }
+}
